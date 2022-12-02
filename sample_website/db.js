a18c222d2e19036be5d512ab8f9b5c59e1698330
Elizabeth Connacher <70343514+econnacher@users.noreply.github.com>
2022-12-02 15:19:10 -0700

    liz comment

--- sample_website/db.js
+++ sample_website/db.js
@@ -2,5 +2,5 @@
 const mongoose = require("mongoose");
 mongoose.connect("mongodb://127.0.0.1/cloud", { useNewUrlParser: true, useUnifiedTopology:true });
 
-
+//hi i added nothing
 module.exports = mongoose;
